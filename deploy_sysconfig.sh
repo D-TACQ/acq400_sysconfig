2eed8e71a68296dad2f7bcba79e08a662f731803
Peter Milne <peter.milne@d-tacq.com>
2016-04-08 11:59:39 +0100

    reworked to handle arbitrary site lists and custom nchannels eg ./deploy_sysconfig.sh acq1001_105 acq480 1 2 3 4 5 6 ./deploy_sysconfig.sh acq1001_105 acq435-24 1 2 3 4
    NB: site-1-peers and transient.init are TEMPORARy files. DO NOT GIT THEM!
    	deleted:    2xacq424_transient.init
    	modified:   acq420_transient.init
    	modified:   acq424_transient.init
    	modified:   acq425_transient.init
    	renamed:    ACQ43X_peers -> acq430-site-1-peers
    	modified:   acq430_transient.init
    	new file:   acq435-site-1-peers
    	modified:   acq435_transient.init
    	new file:   acq437-site-1-peers
    	new file:   acq480-site-1-peers
    	modified:   acq480_transient.init
    	modified:   bolo8_transient.init
    	renamed:    sysconfig/site-1-peers -> default-site-1-peers
    	modified:   deploy_sysconfig.sh

--- deploy_sysconfig.sh
+++ deploy_sysconfig.sh
@@ -1,67 +1,60 @@
 #!/bin/sh
 
-if [[ $# -eq 0 ]] ; then
-    echo -e '\n Enter Carrier followed by Mezzanine\n e.g. acq1001_079 acq420\n'
+if [[ $# -lt 2 ]] ; then
+    echo -e '\n Enter Carrier followed by Mezzanine\n e.g. acq1001_079 acq420 [site1 site2 siteN]\n'
     exit 0
 fi
 
 host=$1
 mezz=$2
+shift;shift
+sites="${*:-1}"
+SITELIST="$(echo $sites | tr \  ,)"
+sitecount=$(echo -n $sites | tr -d \  | wc -c)
 
-echo $host
+
+get_nchan() {
+	mz=$1
+	nc=${mz#*-}
+	if [ $mz == $nc ]; then
+		
+		case $mz in 
+		acq420) nc=4;;
+		acq424) nc=32;;
+		acq425) nc=16;;
+		acq430)	nc=8;;
+		acq435) nc=32;;
+		acq480) nc=8;;
+		bolo8)	nc=8;;
+		*)	echo ERROR: unknown module; exit 1;;
+		esac
+	fi
+	echo $nc	 
+}
+
+MODNAME=${mezz%-*}
+nchan=$(get_nchan $mezz)
+let NCHAN=$nchan*$sitecount
+echo $host $mezz $sites SITELIST:$SITELIST sitecount:$sitecount NCHAN $NCHAN
 
 scp -r sysconfig root@$host:/mnt/local/
 
-case $mezz in
-"acq420")
-  trans_file="acq420_transient.init"
-  echo $trans_file
-  ;;
-"acq425")
-  trans_file="acq425_transient.init"
-  echo $trans_file
-  ;;
-"2xacq425")
-  trans_file="2xacq425_transient.init"
-  echo $trans_file
-  ;;
-"acq424")
-  trans_file="acq424_transient.init"
-  echo $trans_file
-  echo -e "\e[34mN.B. Clock setup in transient file!"; tput sgr0
-  ;;
-"2xacq424")
-  trans_file="2xacq424_transient.init"
-  echo $trans_file
-  echo -e "\e[34mN.B. Clock setup in transient file!"; tput sgr0
-  ;;
-"acq430")
-  trans_file="acq430_transient.init"
-  echo $trans_file
-  ;;
-"acq435")
-  trans_file="acq435_transient.init"
-  echo $trans_file
-  ;;
-"acq480")
-  trans_file="acq480_transient.init"
-  echo $trans_file
-  ;;
-"bolo8")
-  trans_file="bolo8_transient.init"
-  echo $trans_file
-  ;;
-*)
-  echo -e "\nInvalid mezzanine specified!!!\n"
-  echo -e "acq420\nacq425\n2xacq425\nacq424\n2xacq424\nacq430\nacq435\nbolo8\n" 
-  exit 0
-  ;;
-esac
 
-scp $trans_file root@$host:/mnt/local/sysconfig/transient.init
-if [[ $trans_file =~ "acq43" ]]; then
-  scp ACQ43X_peers root@$host:/mnt/local/sysconfig/site-1-peers
+if [ ! -e ${MODNAME}_transient.init ]; then
+	echo ERROR ${MODNAME}_transient.init not found
+	exit 1
 fi
 
-###ssh root@$host /mnt/bin/update_release /tmp/$LATEST
+sed -e "s/%NCHAN%/$NCHAN/g" -e "s/%SITELIST%/$SITELIST/g" \
+	${MODNAME}_transient.init >transient.init
+
+if [ -e ${MODNAME}-site-1-peers ]; then
+	PEERS=${MODNAME}-site-1-peers
+else
+	PEERS=default-site-1-peers
+fi
+
+sed -e "s/%SITELIST%/$SITELIST/g" $PEERS >site-1-peers
+
+scp transient.init site-1-peers root@$host:/mnt/local/sysconfig
 

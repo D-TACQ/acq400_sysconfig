68cdaa13ef5fec1546effe270739d1c3704a0650
dt100 <dt100@d-tacq.com>
2017-02-06 13:16:14 +0000

    OVERSAMPLING VALUE MUST BE NEGATIVE 	modified:   acq430_epics.sh

--- acq430_epics.sh
+++ acq430_epics.sh
@@ -1,4 +1,4 @@
 # AI average over 2048 points, the max for 64K buffer, 8 channels
-echo 2048 > /etc/acq400/0/OVERSAMPLING
+echo -2048 > /etc/acq400/0/OVERSAMPLING
 export EPICS_CA_MAX_ARRAY_BYTES=500000
 

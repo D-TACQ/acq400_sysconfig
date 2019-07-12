eb379b199aa26a42cf493e8350c0408e77a24654
dt100 <dt100@d-tacq.com>
2019-07-12 17:06:17 +0100

    pgm: rework to deploy to ./STAGING before scp in one go at the end DRYRUN=1 : no scp, for testing and creating a config for an absent UUT  (eg UUT has been upgraded in the field, new latest config required) ACQ1014=1 : create acq1014 pair to UUT, UUT+1
    	modified:   deploy_sysconfig.sh

--- deploy_sysconfig.sh
+++ deploy_sysconfig.sh
@@ -2,27 +2,31 @@
 
 if [[ $# -lt 2 ]] ; then
     echo -e '\n Enter Carrier followed by Mezzanine\n e.g. acq1001_079 acq420 [site1 site2 siteN]\n'
+    echo -e '\n FOR ACQ1014 run ACQ1014=1 ./deploy_sysconfig acq1001_LEFT acq480 .. assumes acq1001_RIGHT is +1\n'
+    echo -e '\n FOR DRYRUN run DRYRUN=1 ./deploy_sysconfig xxxx and examine ./STAGING\n'
     exit 0
 fi
 
 host=$1
 carr=${host:3:4}
-echo $carr
-# For use in 1014 systems
-#host2=${host: -3};host2=$((host2+1));host2=(${host: 0:8}$host2)
+
+ACQ1014=${ACQ1014:-0}
+if [ $ACQ1014 == 1 ]; then
+	host2=${host: -3};host2=$((host2+1));host2=(${host: 0:8}$host2)
+	echo ACQ1014 $host host2 configured $host2
+fi
 mezz=$2
 shift;shift
 sites="${*:-1}"
 SITELIST="$(echo $sites | tr \  ,)"
 sitecount=$(echo -n $sites | tr -d \  | wc -c)
-custom_flag=0
-if [[ $host =~ "kmcu" ]]; then
-	custom_rc=1
-else
-	custom_rc=0
-fi
-debug=0
+debug=${DRYRUN:-0}
 
+
+echo "CLEANUP rm STAGING"
+rm -Rf STAGING STAGING2
+mkdir -p STAGING/mnt/local/cal 
+echo STAGING is a place to build a local copy of the remote image
 
 get_nchan() {
 	mz=$1
@@ -82,11 +86,17 @@ get_sr() {
 MODNAME=${mezz%-*}
 nchan=$(get_nchan $mezz)
 samp_rate=$(get_sr $mezz)
-echo $samp_rate
+echo "DEBUG SR $samp_rate"
 let NCHAN=$nchan*$sitecount
-echo $host $mezz $sites SITELIST:$SITELIST sitecount:$sitecount NCHAN $NCHAN
+echo "DEBUG host $host mezz $mezz $sites SITELIST:$SITELIST sitecount:$sitecount NCHAN $NCHAN"
 
-if [ $debug == 0 ]; then scp -r sysconfig root@$host:/mnt/local/;fi
+# set some defaults in STAGING. Maybe they get overwritten
+cp -r sysconfig STAGING/mnt/local
+
+if [[ $host =~ "kmcu" ]]; then
+	echo WORKTODO : kmcu does NOT mess with rc.user, make your own
+	rm STAGING/mnt/local/rc.user
+fi
 
 case $mezz in
 "acq420"|"acq423"|"acq427")
@@ -94,16 +104,16 @@ case $mezz in
   ;;
 "acq425"|"acq424")
   trans_file="acq42X_transient.init"
-  custom_flag=1
+  [ $sitecount -gt 2 ] && cp acq42X_AXI_DMA_BUFFERS root@$host:/mnt/local/sysconfig/acq400.sh
   ;;
 "acq425-18")
   trans_file="acq43X_transient.init"
-  custom_flag=1
+  [ $sitecount -gt 2 ] && cp acq42X_AXI_DMA_BUFFERS root@$host:/mnt/local/sysconfig/acq400.sh
   ;;
 "acq430")
   trans_file="acq43X_transient.init"
-  scp acq430_epics.sh root@$host:/mnt/local/sysconfig/epics.sh
-  scp acq430_acq420_custom root@$host:/mnt/local/acq420_custom
+  cp acq430_epics.sh STAGING/mnt/local/sysconfig/epics.sh
+  cp acq430_acq420_custom STAGING/mnt/local/acq420_custom
   ;;
 "acq435"|"acq437")
   trans_file="acq43X_transient.init"
@@ -113,7 +123,6 @@ case $mezz in
   ;;
 "acq480")
      trans_file="acq480_transient.init"
-     custom_rc=0
      if [ $debug == 0 ] ; then
           if [[ $host =~ "acq1001" ]]; then
                ssh root@$host grep devicetree_image /tmp/u-boot_env | grep -q 1014
@@ -121,22 +130,21 @@ case $mezz in
                     echo +++ acq1014 found
                     echo -e "\e[34mCopying config files to both $host and $host2"; tput sgr0
                     sed -e "s/%MAST_HOST%/$host2/g" acq1014_epics_mirror_def > acq1014_epics_mirror
-                    scp acq480_1014_rc.user root@$host:/mnt/local/rc.user
-                    scp acq480_1014_rc.user root@$host2:/mnt/local/rc.user
-                    scp acq1014_epics_mirror root@$host:/mnt/local/sysconfig/epics.sh
-                    scp acq1014_epics_mirror_slave root@$host2:/mnt/local/sysconfig/epics.sh
-                    scp acq1001_acq480_bos.sh root@$host:/mnt/local/sysconfig/bos.sh
-                    scp acq1001_acq480_bos.sh root@$host2:/mnt/local/sysconfig/bos.sh
-                    scp acq1001_acq480_acq420_custom root@$host:/mnt/local/acq420_custom
-                    scp acq1001_acq480_acq420_custom root@$host2:/mnt/local/acq420_custom
+		    mkdir STAGING STAGING2
+		    for st in STAGING STAGING2; do
+	            	cp acq480_1014_rc.user ${st}/mnt/local/rc.user
+                    	cp acq1014_epics_mirror ${st}/mnt/local/sysconfig/epics.sh
+                    	cp acq1001_acq480_bos.sh ${st}/mnt/local/sysconfig/bos.sh
+                    	cp acq1001_acq480_acq420_custom ${st}/mnt/local/acq420_custom
+		    done
                else
-                    #scp acq480_1001_rc.user root@$host:/mnt/local/rc.user
-                    scp acq480_rc.user root@$host:/mnt/local/rc.user
-                    scp acq1001_acq480_bos.sh root@$host:/mnt/local/sysconfig/bos.sh
-                    scp acq1001_acq480_acq420_custom root@$host:/mnt/local/acq420_custom
+                    #cp acq480_1001_rc.user STAGING/mnt/local/rc.user
+                    cp acq480_rc.user STAGING/mnt/local/rc.user
+                    cp acq1001_acq480_bos.sh STAGING/mnt/local/sysconfig/bos.sh
+                    cp acq1001_acq480_acq420_custom STAGING/mnt/local/acq420_custom
                fi
           else
-               scp acq480_rc.user root@$host:/mnt/local/rc.user
+               cp acq480_rc.user STAGING/mnt/local/rc.user
           fi
      fi
   ;;
@@ -144,10 +152,10 @@ case $mezz in
   trans_file="bolo8_transient.init"
   ;;
 "dio432")
-  trans_file="acq43X_transient.init"
   custom_rc=1
-  scp dio432_rc.user root@$host:/mnt/local/rc.user
-  scp DO_acq420_custom root@$host:/mnt/local/acq420_custom
+  trans_file="acq43X_transient.init"
+  cp dio432_rc.user STAGING/mnt/local/rc.user
+  cp DO_acq420_custom STAGING/mnt/local/acq420_custom
   ;;
 *)
   echo -e "\nInvalid mezzanine specified!!!\n"
@@ -156,10 +164,9 @@ case $mezz in
   ;;
 esac
 
-echo $trans_file
-echo $MODNAME
+echo "DEBUG trans_file $trans_file MODNAME $MODNAME"
 if [ ! -e ${MODNAME}_transient.init ]; then
-	echo Warning : ${MODNAME}_transient.init not found
+	echo "Warning : ${MODNAME}_transient.init not found .. but that is probably OK"
 	#exit 1
 fi
 
@@ -182,7 +189,8 @@ sed -e "s/%SITELIST%/$SITELIST/g" $PEERS >site-1-peers
 ###
 # Sed into the template rc.user file to generate board specific clocking
 ###
-if [ $custom_rc == 0 ]; then
+cmp -s rc.user STAGING/mnt/local/rc.user
+if [ ! -e STAGING/mnt/local/rc.user ]; then
 	if [[ $mezz =~ "acq43" ]]; then
 		acq_sub="acq43x"
 		setp=$samp_rate
@@ -198,28 +206,31 @@ if [ $custom_rc == 0 ]; then
 		acq_sub="acq480"
 		setp=$samp_rate
 	fi
-	echo $acq_sub
 	if [ $carr == "2106" ]; then
 		setp=$samp_rate
 	fi
+	echo "DEBUG acq_sub $acq_sub setp $setp"
 	sed -e "s/%MEZZ%/$mezz/g" -e "s/%STR_SR%/$samp_rate/g" -e "s/%CARRIER%/$carr/g" \
 		-e "s/%ACQSUB%/$acq_sub/g" -e "s/%SETPOINT%/$setp/g" \
-		template_rc.user > rc.user
+		template_rc.user > STAGING/mnt/local/rc.user
 fi
 
+staging=STAGING
+[ -e STAGING2 ] && staging="$staging STAGING2"
+
+uut=$host
+for st in $staging; do
+        sed -ie "2i# created by deploy_sysconfig uut:$uut mezz:$mezz\n# ${USER}@$(hostname) $(date)\n" $st/mnt/local/rc.user
+	cp transient.init site-1-peers $st/mnt/local/sysconfig
+	uut=$host2
+done
 
 ###
 # Copy files to UUT
 ###
-if [ $debug == 0 ] ; then
-	if [[ $(ssh root@$host 'if [ ! -d /mnt/local/cal ]; then echo 1;fi') = "1" ]]; then # If cal does not exist
-		scp -r cal root@$host:/mnt/local/
-	fi
-	scp transient.init site-1-peers root@$host:/mnt/local/sysconfig
-	if [ $custom_flag == 1 ]; then # Custom AXI buffers length
-	   scp acq42X_AXI_DMA_BUFFERS root@$host:/mnt/local/sysconfig/acq400.sh
-	fi
-	if [ $custom_rc == 0 ]; then
-	   scp rc.user root@$host:/mnt/local/rc.user
-	fi
+if [ $debug == 0 ]; then
+	scp -r STAGING/mnt/* root@$host:/mnt/
+	[ "x$host2" != "x" ] && scp -r STAGING2/mnt/* root@$host2:/mnt/
+else
+	echo debug mode no deploy. Look in ./STAGING for details
 fi

359989dfa4d6f0d579a8e383d155aa3a6b889e7d
dt100 <dt100@d-tacq.com>
2019-11-19 11:37:50 +0000

    Comment out the spectrum stuff. Causing CPU load problems in its current state

--- sysconfig/epics.sh
+++ sysconfig/epics.sh
@@ -1,6 +1,6 @@
 echo -1 >/etc/acq400/0/OVERSAMPLING
 export EPICS_CA_MAX_ARRAY_BYTES=500000
-export IOC_PREINIT=./scripts/load.SpecReal
-[ -e /dev/shm/window ] || \
-	ln -s /usr/local/CARE/hanning-float.bin /dev/shm/window
+#export IOC_PREINIT=./scripts/load.SpecReal
+#[ -e /dev/shm/window ] || \
+#	ln -s /usr/local/CARE/hanning-float.bin /dev/shm/window
 

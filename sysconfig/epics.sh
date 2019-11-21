5cd1072436d569831715a341b406a53388444e6a
dt100 <dt100@d-tacq.com>
2019-11-21 10:47:29 +0000

    document spectrum comment out "not for production use" 	modified:   sysconfig/epics.sh

--- sysconfig/epics.sh
+++ sysconfig/epics.sh
@@ -1,5 +1,7 @@
 echo -1 >/etc/acq400/0/OVERSAMPLING
 export EPICS_CA_MAX_ARRAY_BYTES=500000
+# uncomment for live spectra, but not recommended for production use
+# as it can result in loss of data in some conditions
 #export IOC_PREINIT=./scripts/load.SpecReal
 #[ -e /dev/shm/window ] || \
 #	ln -s /usr/local/CARE/hanning-float.bin /dev/shm/window

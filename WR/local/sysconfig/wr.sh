4fc703f4fb0fa96e505dde3d3d0b83b96b6bd813
dt100 <dt100@d-tacq.com>
2020-01-30 10:46:40 +0000

    Adding new WR stuff into commit so we can make clean and deploy again

---
+++ WR/local/sysconfig/wr.sh
@@ -0,0 +1,8 @@
+WRTD_TICKNS=50
+WRTD_DNS=50000000
+WRTD_VERBOSE=1
+# we're going to run TX on demand to avoid multi-box race when trigger continuous
+WRTD_TX=0
+#WRTD_RX_MATCHES=dog,cat,rabbit,mouse
+#export WRTD_RX_MATCHES=dog,cat,rabbit,mouse,acq2106_133.0
+

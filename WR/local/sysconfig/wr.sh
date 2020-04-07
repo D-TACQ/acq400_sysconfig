9ed15dcb70e8cbeb2d81e03449f3494a8a855587
dt100 <dt100@d-tacq.com>
2020-04-07 22:36:59 +0100

    WRTD_DELTA_NS replaces WRTD_DNS intermediate step.. 	modified:   WR/local/sysconfig/wr.sh

--- WR/local/sysconfig/wr.sh
+++ WR/local/sysconfig/wr.sh
@@ -2,7 +2,7 @@
 WRTD_TICKNS=25
 # time delay on trigger, 50msec is safe
 WRTD_DELTA_NS=50000000
-WRTD_DNS=WRTD_DELTA_NS
+WRTD_DNS=$WRTD_DELTA_NS
 WRTD_VERBOSE=0
 # we're going to run TX on demand to avoid multi-box race when trigger continuous
 WRTD_TX=0

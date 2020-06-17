b47c3300f5da54c880c4dbc2f8155db0799102de
dt100 <dt100@d-tacq.com>
2020-06-17 21:10:07 +0100

    expunge WRTD_DNS, completely replaced by WRTD_DELTA_NS 	modified:   WR/local/sysconfig/wr.sh

--- WR/local/sysconfig/wr.sh
+++ WR/local/sysconfig/wr.sh
@@ -2,7 +2,6 @@
 WRTD_TICKNS=25
 # time delay on trigger, 50msec is safe
 WRTD_DELTA_NS=50000000
-WRTD_DNS=$WRTD_DELTA_NS
 WRTD_VERBOSE=0
 # we're going to run TX on demand to avoid multi-box race when trigger continuous
 WRTD_RX=1

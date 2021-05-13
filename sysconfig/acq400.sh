4f7723719374f8e759123ee87e99f405dc9b130c
dt100 <dt100@d-tacq.com>
2021-05-13 09:04:50 +0100

    add further option hints data from [dt100@eigg-fs CAL]$ find . -name acq400.sh | xargs cat | sort | uniq 	modified:   sysconfig/acq400.sh

--- sysconfig/acq400.sh
+++ sysconfig/acq400.sh
@@ -1,3 +1,14 @@
+# external reboot control 
 REBOOT_KNOB=y
+# optional external rsyslog HOST
 # RSYSLOG_HOST=naboo
+# optional, expert tailor number of buffers
+# NBUF=512
+# optional, expert tailor buffer allocated length
+# BLEN=1045876
+# optional, specialist: control MULTI_EVENT
+#MULTI_EVENT=100000,100000,100,0
+#MULTI_EVENT_DISK=yes
+
+
 

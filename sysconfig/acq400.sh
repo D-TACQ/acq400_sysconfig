ced6c0f1f187b789ae631afede75e2c5d34e01c9
dt100 <dt100@d-tacq.com>
2021-06-10 15:01:48 +0100

    ETH1_1000LX option documented 	modified:   sysconfig/acq400.sh

--- sysconfig/acq400.sh
+++ sysconfig/acq400.sh
@@ -9,6 +9,12 @@ REBOOT_KNOB=y
 # optional, specialist: control MULTI_EVENT
 #MULTI_EVENT=100000,100000,100,0
 #MULTI_EVENT_DISK=yes
+# scaled rate status for CONTINUOUS OPI
+#STREAM_OPTS=--fill-scale
+# ETH1000LX - connect to SFPD (MGT482 systems only)
+#ETH1_E1000X=y
+
+
 
 
 

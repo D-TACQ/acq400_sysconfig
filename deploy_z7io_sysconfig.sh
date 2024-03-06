9cd92d5e947d26670091c573ad7103122502d3c0
dt100 <dt100@d-tacq.com>
2024-03-06 09:11:09 +0000

    added z7io configs 	modified:   deploy_z7io_sysconfig.sh 	new file:   z7io/z7io_04_04/local/rc.user 	new file:   z7io/z7io_04_41/local/rc.user

--- deploy_z7io_sysconfig.sh
+++ deploy_z7io_sysconfig.sh
@@ -1,2 +1,44 @@
 #!/bin/bash
-echo new scheme coming
+
+
+if [[ $# -lt 2 ]]; then
+	cat - <<EOF
+		./deploy_z7io_sysconfig.sh z7io_123 M1 M2 [M3]
+		where M1 is the MCODE module in site 1, M2 in site 2, M3 in site 3
+		site 3 is the AMC FMC
+
+		eg
+		./deploy_z7io_sysconfig.sh z7io_123 04 04
+		./deploy_z7io_sysconfig.sh z7io_123 04 04 40
+		./deploy_z7io_sysconfig.sh z7io_123 04 41
+EOF
+	exit 	0
+fi
+
+DRYRUN=${DRYRUN:-0}
+
+host=$1
+if [ "${host:0:4}" != "z7io" ]; then
+	echo ERROR host $host is not a z7io
+	exit 1
+fi
+
+shift
+signature="./z7io/z7io"
+for ax in $*; do
+	signature="${signature}_$ax"
+done
+
+if [ ! -d $signature/local ]; then
+	echo ERROR SIGNATURE $signature/local does not exist
+	exit 1
+fi
+
+echo SIGNATURE $signature/local exists
+[ $DRYRUN -ne 0 ] && echo DRYRUN scp -r $signature/local/* root@$host:/mnt/local
+[ $DRYRUN -eq 0 ] && scp -r $signature/local/* root@$host:/mnt/local
+
+
+
+
+
---
+++ z7io/z7io_04_41/local/sysconfig/epics.sh
@@ -0,0 +1,34 @@
+echo -1 >/etc/acq400/0/OVERSAMPLING
+export EPICS_CA_MAX_ARRAY_BYTES=500000
+export SIZE=512
+# uncomment for live spectra, but not recommended for production use
+# as it can result in loss of data in some conditions
+#export IOC_PREINIT=./scripts/load.SpecReal
+#[ -e /dev/shm/window ] || \
+#	ln -s /usr/local/CARE/hanning-float.bin /dev/shm/window
+
+# if we have two Ethernets,restrict CA to eth0, otherwise, leave ioc to work it out
+ETH1=$(get-ip-address eth1)
+if [ $? -eq 0 ]; then
+	ETH0=$(get-ip-address eth0)
+	if [ $? -eq 0 ]; then
+		export EPICS_CAS_INTF_ADDR_LIST="$ETH0 $(get-ip-address lo)"
+	fi
+fi
+
+
+judgement() {
+# short trace length, rapid update 50Hz possible
+# $1:size, $2:dX (don't care) $3:BPB
+export SIZE=${1:-128}
+export IOC_PREINIT=./scripts/load.judgement
+export acq400_Judgement_FIRST_SAM=1
+export BURSTS_PER_BUFFER=${3:-1}
+export RTM_BUFFER_MON=y
+export RTM_BUFFER_MON_VERBOSE=1
+export WIN_R_LIM=6
+}
+source /mnt/local/sysconfig/acq400.sh
+[ ! -z "$ACQ400_JUDGEMENT" ] && judgement $ACQ400_JUDGEMENT
+
+

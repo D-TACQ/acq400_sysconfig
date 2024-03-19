23f220335e8bb0e26dfb5285de85423390615846
dt100 <dt100@d-tacq.com>
2024-03-19 14:43:44 +0000

    enumerate judgement opts 	modified:   z7io/z7io_08_08_40/local/sysconfig/acq400.sh

--- z7io/z7io_08_08_40/local/sysconfig/acq400.sh
+++ z7io/z7io_08_08_40/local/sysconfig/acq400.sh
@@ -9,7 +9,13 @@ NBUF=128
 STREAM_OPTS=--full-copy
 
 
+# for "normal use", comment out ALL ACQ400_JUDGMENT
+# rapid update, ext trigger
 ACQ400_JUDGEMENT="256 d0"
+# rapid update, int trigger
+#ACQ400_JUDGEMENT="256 d1"
+# < 10Hz, int trigger
+#ACQ400_JUDGEMENT="2048 d1"
 
 FANSPEED=100
 

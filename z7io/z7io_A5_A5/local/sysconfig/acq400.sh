327b4b8353e1e168fe40441b6663745f3a465ff8
dt100 <dt100@d-tacq.com>
2024-08-07 17:06:52 +0100

    z7io_09_09 and z7io_A5_A5 same as ACQ424, but different clocks and, for A5, NCHAN 	new file:   z7io/z7io_09_09/local/rc.user 	new file:   z7io/z7io_09_09/local/sysconfig/acq400.sh 	new file:   z7io/z7io_09_09/local/sysconfig/bos.sh 	new file:   z7io/z7io_09_09/local/sysconfig/epics.sh 	new file:   z7io/z7io_09_09/local/sysconfig/site-1-peers 	new file:   z7io/z7io_09_09/local/sysconfig/transient.init 	new file:   z7io/z7io_A5_A5/local/rc.user 	new file:   z7io/z7io_A5_A5/local/sysconfig/acq400.sh 	new file:   z7io/z7io_A5_A5/local/sysconfig/bos.sh 	new file:   z7io/z7io_A5_A5/local/sysconfig/epics.sh 	new file:   z7io/z7io_A5_A5/local/sysconfig/site-1-peers 	new file:   z7io/z7io_A5_A5/local/sysconfig/transient.init

---
+++ z7io/z7io_A5_A5/local/sysconfig/acq400.sh
@@ -0,0 +1,20 @@
+# remote network boot (superceded by MTCA/mmc)
+REBOOT_KNOB=y
+# divide DRAM into 128 x 4MB buffers (large buffers best for AWG)
+BLEN=4194304
+NBUF=128
+#BLEN=1048576
+#NBUF=512
+# scaled rate status for CONTINUOUS OPI
+#STREAM_OPTS=--fill-scale
+#STREAM_OPTS=--null-copy
+
+# Rapid Scope, 256 points, external trigger to 50Hz
+ACQ400_JUDGEMENT="256 d0"
+# Rapid Scope, 256 points, internal trigger to 50Hz
+#ACQ400_JUDGEMENT="256 d1"
+# Rapid Scope, 1024 points, internal trigger to 10Hz
+#ACQ400_JUDGEMENT="1024 d1"
+# for normal streaming, leave all JUDGEMENT commented out
+
+
